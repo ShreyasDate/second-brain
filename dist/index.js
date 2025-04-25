1bcf220167153ff96be5c1ffba397a89e3b9dbf4
ShreyasDate <shreyas.date09@gmail.com>
2025-04-25 18:34:30 +0530

    signin endpoint added

--- dist/index.js
+++ dist/index.js
@@ -14,6 +14,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const db_1 = require("./db");
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const JWT_SECRET = "ahdjfauregfgafrgayg";
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -32,6 +34,32 @@ app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* ()
         res.status(403).json({
             message: "duplicate entry or error",
             error: error
+        });
+    }
+}));
+app.post("/signin", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+        const userExist = yield db_1.UserModel.findOne({
+            username: username,
+            password: password
+        });
+        if (userExist) {
+            const token = jsonwebtoken_1.default.sign({ username }, JWT_SECRET);
+            res.status(200).json({
+                token: token
+            });
+        }
+        else {
+            res.status(403).json({
+                message: "incorrect credentials"
+            });
+        }
+    }
+    catch (error) {
+        res.status(403).json({
+            error
         });
     }
 }));
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { UserModel } from "./db";
+import  jwt  from "jsonwebtoken";
 
+
+const JWT_SECRET = "ahdjfauregfgafrgayg"
 const app = express();
 
 app.use(express.json())
@@ -27,6 +30,34 @@ app.post("/signup",async (req,res)=>{
         })
     }
 
- })
+})
+
+app.post("/signin",async (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        const userExist = await UserModel.findOne({
+            username : username,
+            password: password
+        })
+        
+        if(userExist){
+            const token = jwt.sign({username},JWT_SECRET)
+            res.status(200).json({
+                token : token
+            })
+        }else{
+            res.status(403).json({
+                message : "incorrect credentials"
+            })
+        }
+    } catch (error) {
+        res.status(403).json({
+            error
+        })
+    }
+    
+})
 
 app.listen(3000)
