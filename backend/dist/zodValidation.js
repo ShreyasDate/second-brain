8eb3a05d9f814bbd83c056ba44404a82fc6a88ab
ShreyasDate <shreyas.date09@gmail.com>
2025-09-03 19:36:16 +0530

    Remove dist folder from tracking

--- backend/dist/zodValidation.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.passwordSchema = exports.usernameSchema = void 0;
-const zod_1 = require("zod");
-exports.usernameSchema = zod_1.z.string().min(3);
-exports.passwordSchema = zod_1.z.string().min(3);
